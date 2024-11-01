1a3149102fea82550b2783dda5feef7225510444
Gowtham Ashok <gowthamashok20@gmail.com>
2024-11-01 15:25:58 -0400

    add CORS middleware to server

--- server/main.py
+++ server/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from glob import glob
 from random import randint
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Query(BaseModel):
     query: str
